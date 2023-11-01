a33a50ef263fa4a40dc1687df841aceb543fd5f5
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-01 16:33:11 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+
+if (mainElement) {mainElement.remove();
+}
+ const newHeader = document.createElement("h1");
+ newHeader.textContent = "Katie is the champion";
+newHeader.id = "victory";
+   
+document.body.appendChild(newHeader);
